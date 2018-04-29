39e2c21137412e8895e6b78726b1be71219922bf
Yoann Chane Kive <yoannck@gmail.com>
2018-04-29 10:53:38 +0200

    convert

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
+let existedURLS = [];
 chrome.webRequest.onBeforeRequest.addListener((requestDetails) => {
-  if (requestDetails.url.search('.mp3') > -1) {
+  if (requestDetails.url.search('.mp3') > -1 && !existedURLS.includes(requestDetails.url)) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      existedURLS.push(requestDetails.url);
       chrome.tabs.sendMessage(tabs[0].id, {action: "accessSoundsDL", url: requestDetails.url}, function(response) {});
     });
   }
--- main.js
+++ main.js
@@ -2,7 +2,6 @@
  * Copyright(c) 2018 Yoann Chane Kive
  * MIT Licensed
  */
-
 console.log('Extension Launch!');
 window.onload = function() {
   setTimeout(function(){
@@ -10,26 +9,24 @@ window.onload = function() {
     const samples = document.querySelectorAll('.SoundTable_Play');
     // chrome.extension.getURL("icons/Icon-16.png")
     samples.forEach( sample => {
-      sample.insertAdjacentHTML('beforeend', '<a id="DL'+ sample.getAttribute('data-tmeta') +'" target="_blank" download><img style="width:15px;height:15px;padding-left:10px;" src="https://cdn2.iconfinder.com/data/icons/online-communication-i/24/Material_icons-04-16-128.png" /></a>');
-      // let inc = 0;
-      // sample.parentElement.addEventListener('mouseover', () => {
-      //   if (inc++ === 0) {
-      //     // console.log('intercept url');
-      //     // console.log('init the link');
-      //   }
-      // });
-
+      sample.insertAdjacentHTML('beforeend', '<a id="DL'+ sample.getAttribute('data-tmeta') +'" download><img style="width:15px;height:15px;padding-left:10px;" src="https://cdn2.iconfinder.com/data/icons/online-communication-i/24/Material_icons-04-16-128.png" /></a>');
     });
 
     chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
       if (msg.action == 'accessSoundsDL') {
         console.log(msg.url);
-        document.querySelector('.SoundTable_Container:hover a').href = msg.url;
-        // document.querySelector('.SoundTable_Container:hover a').download = document.querySelector('.SoundTable_Container:hover .SoundTable_SoundLink').textContent;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', msg.url, true);
+        xhr.responseType = 'blob'; //blob
+        xhr.onload = () => {
+          let link = document.querySelector('.SoundTable_Container:hover a');
+          link.href = window.URL.createObjectURL(xhr.response);
+          link.download = `${document.querySelector('.SoundTable_Container:hover .SoundTable_SingleColumnMetaContainer').textContent}.mp3`;
+        };
+        xhr.send();
       }
     });
-
-
-  }, 2000);
+  }, 3000);
 
 };
